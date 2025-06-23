d473ef12eb09dd39820525a03815027be47f09df
bigilisandro <bigilisandro@gmail.com>
2025-06-23 15:01:15 -0300

    add verified contract

--- SimpleSwap.sol
+++ SimpleSwap.sol
@@ -2,6 +2,21 @@
 pragma solidity ^0.8.20;
 
 /**
+ * @title IERC20
+ * @dev Interface for the ERC20 standard.
+ */
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
  * @title SimpleSwap
  * @author Lisandro Bigi
  * @dev This contract allows users to swap tokens, add/remove liquidity, and query prices.
